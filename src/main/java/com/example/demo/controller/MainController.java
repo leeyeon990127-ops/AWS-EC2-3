8e3241885b910f635ecb87841c98a455e07d36f1
yeonsoo <leeyeon990127@gmail.com>
2026-08-13 15:21:42 +0900

    build.yml

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @RestController
+
+    public class MainController {
+        @GetMapping("/")
+        public String index() {
+            return "Hello World - /";
+        }
+
+        @GetMapping("/user")
+        public String user() {
+            return "Hi ME - /";
+        }
+    }
+
